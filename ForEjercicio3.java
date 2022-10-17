c6712a71b93342c452675e6868c0e1aa0636852b
CarVT <114205382+CarVT@users.noreply.github.com>
2022-10-16 23:07:44 -0500

    Add files via upload

---
+++ ForEjercicio3.java
@@ -0,0 +1,13 @@
+public class ForEjercicio3 {
+    public static void main(String[] args) {
+
+        String[] nombres = new String[]{"Pepe", "Juan", "Ana"};
+
+
+        for(int i = 0; i < nombres.length; i++) {
+            System.out.print(nombres[i] + " ");
+        }
+}
+
+}
+
